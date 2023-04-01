5a400fe99e84814b46b6545ad89b546708bca9cd
Yumin Cho <dbals081216@gmail.com>
2023-04-02 03:59:22 +0900

    done tutorial 8

--- lib/main.dart
+++ lib/main.dart
@@ -115,7 +115,8 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder(); // FavoritesPage가 들어갈 예정
+        // page = Placeholder(); // FavoritesPage가 들어갈 예정
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -210,6 +211,35 @@ class GeneratorPage extends StatelessWidget {
     );
   }
 }
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView( // scroll 가능한 list view
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+          '${appState.favorites.length} favorites:'),
+          ),
+          for (var pair in appState.favorites)
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text(pair.asLowerCase),
+            )
+
+      ],
+    );
+  }
+} 
 
 class BigCard extends StatelessWidget { // assigned name "BigCard" to new class
   const BigCard({
